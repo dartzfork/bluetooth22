e1be25250640abcb59f94e3229cb6a6c921603d0
dartzfork <dartzcvm@proton.me>
2025-07-12 23:58:02 +0000

    Add files via upload

---
+++ stage.js
@@ -0,0 +1,32 @@
+const io = require("socket.io-client");
+const socket = io("https://www.windows93.net:8088", {
+	forceNew: true,
+	transportOptions: {
+		polling: {
+			extraHeaders: {
+				"Accept": "*/*",
+				"Accept-Encoding": "identity",
+				"Accept-Language": "*",
+				"Cache-Control": "no-cache",
+				"Connection": "keep-alive",
+				"Cookie": "",
+				"Host": "www.windows93.net",
+				"Origin": "http://www.windows93.net",
+				"Pragma": "no-cache",
+				"Referer": 'http://www.windows93.net/trollbox/index.php',
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+			}
+		}
+	}
+});
+
+        socket.emit("user joined", "N0rmalUs3r", "green", "", "");
+
+        socket.on("message", function(data) {
+        console.log(data.nick + " Said: " + data.msg)
+socket.send(`${Math.random(1)} LOL FUCK THIS`)
+            if (data.msg == "t!hello") {
+                socket.send("Hello, "+data.nick+"!")
+            }
+
+            })
